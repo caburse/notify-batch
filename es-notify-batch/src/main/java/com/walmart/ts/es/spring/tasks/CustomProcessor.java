a020f5f6e7be13943a9b393e7ebc1fce1b814f11
caburse <caburse@wal-mart.com>
2014-07-15 07:32:15 -0500

    changed process methods input to list

--- es-notify-batch/src/main/java/com/walmart/ts/es/spring/tasks/CustomProcessor.java
+++ es-notify-batch/src/main/java/com/walmart/ts/es/spring/tasks/CustomProcessor.java
@@ -1,6 +1,8 @@
 package com.walmart.ts.es.spring.tasks;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.elasticsearch.action.search.SearchResponse;
@@ -19,58 +21,59 @@ import com.walmart.ts.es.model.Alert;
 import com.walmart.ts.es.model.DurationType;
 import com.walmart.ts.es.util.PropertyUtil;
 
-public class CustomProcessor implements ItemProcessor<Alert,Alert> {
+public class CustomProcessor implements ItemProcessor<List<Alert>,List<Alert>> {
 	
 	private static final Logger LOGGER = Logger.getLogger(CustomProcessor.class);
 	
 	@Override
-	public Alert process(Alert alert) throws Exception {
-				
-		Calendar cal = Calendar.getInstance();
-		long end = cal.getTimeInMillis();
-		
-		//Subtract duration of DurationType from current time.
-		cal.set(DurationType.fromString(alert.getAlertDurationType()).getType(), 
-				cal.get(
-						DurationType.fromString(alert.getAlertDurationType()).getType()) 
-						- 
-						alert.getAlertDuration());
-				
-		long begin = cal.getTimeInMillis();
-		
-		Settings settings = ImmutableSettings.settingsBuilder()
-				.put(Constants.CLUSTER_NAME,
-						PropertyUtil.getInstance().getString(PropertiesConstants.ES_CLUSTER))
-				.put(Constants.CLIENT_TRANSPORT, true)
-				.build();
-		
-		Client client = new TransportClient(settings)
-		.addTransportAddress(
-				new InetSocketTransportAddress(
-						PropertyUtil.getInstance().getString(PropertiesConstants.ES_SERVER),
-						PropertyUtil.getInstance().getInt(PropertiesConstants.ES_PORT)));
-		
-		QueryBuilder query = QueryBuilders.boolQuery()
-				.must(QueryBuilders.matchQuery(Constants.FIELD_SOURCE,alert.getSource()))
-				.must(QueryBuilders.matchQuery(alert.getFieldKey(),alert.getFieldValue()))
-//				.must(QueryBuilders.matchAllQuery())
-				.must(QueryBuilders.rangeQuery(Constants.FIELD_TIMESTAMP)
-							.from(begin)
-							.to(end));
-				
-		SearchResponse response = client.prepareSearch()				
-				.setQuery(query)
-				.execute().actionGet();
-		
-		LOGGER.info("response: " + response);
-		LOGGER.info("total hits: " + response.getHits().getTotalHits());
-		
-		client.close();
-		boolean notify = response.getHits().getTotalHits() > alert.getMaxOccurance();
-		if(notify){
-			return alert;							
-		}else{
-			return null;
+	public List<Alert> process(List<Alert> alerts) throws Exception {
+		List<Alert> notifications = new ArrayList<Alert>();
+		for(Alert alert : alerts){
+			Calendar cal = Calendar.getInstance();
+			long end = cal.getTimeInMillis();
+			
+			//Subtract duration of DurationType from current time.
+			cal.set(DurationType.fromString(alert.getAlertDurationType()).getType(), 
+					cal.get(
+							DurationType.fromString(alert.getAlertDurationType()).getType()) 
+							- 
+							alert.getAlertDuration());
+					
+			long begin = cal.getTimeInMillis();
+			
+			Settings settings = ImmutableSettings.settingsBuilder()
+					.put(Constants.CLUSTER_NAME,
+							PropertyUtil.getInstance().getString(PropertiesConstants.ES_CLUSTER))
+					.put(Constants.CLIENT_TRANSPORT, true)
+					.build();
+			
+			Client client = new TransportClient(settings)
+			.addTransportAddress(
+					new InetSocketTransportAddress(
+							PropertyUtil.getInstance().getString(PropertiesConstants.ES_SERVER),
+							PropertyUtil.getInstance().getInt(PropertiesConstants.ES_PORT)));
+			
+			QueryBuilder query = QueryBuilders.boolQuery()
+					.must(QueryBuilders.matchQuery(Constants.FIELD_SOURCE,alert.getSource()))
+					.must(QueryBuilders.matchQuery(alert.getFieldKey(),alert.getFieldValue()))
+	//				.must(QueryBuilders.matchAllQuery())
+					.must(QueryBuilders.rangeQuery(Constants.FIELD_TIMESTAMP)
+								.from(begin)
+								.to(end));
+					
+			SearchResponse response = client.prepareSearch()				
+					.setQuery(query)
+					.execute().actionGet();
+			
+			LOGGER.info("response: " + response);
+			LOGGER.info("total hits: " + response.getHits().getTotalHits());
+			
+			client.close();
+			boolean notify = response.getHits().getTotalHits() > alert.getMaxOccurance();
+			if(notify){
+				notifications.add(alert);							
+			}
 		}
+		return notifications;
 	}
 }
